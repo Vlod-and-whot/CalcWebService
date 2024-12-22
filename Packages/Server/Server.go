34361d98a6c63e505f10def265daead58a7dee1f
Mega_Axolotl <85391186+Vlod-and-whot@users.noreply.github.com>
2024-12-23 00:50:03 +0200

    Update Server.go

--- Packages/Server/Server.go
+++ Packages/Server/Server.go
@@ -16,10 +16,12 @@ type Request struct {
 
 type ResponseSuccess struct {
 	Result string `json:"result"`
+	Code  int    `json:"code"`
 }
 
 type ResponseError struct {
 	Error string `json:"error"`
+	Code  int    `json:"code"`
 }
 
 type Config struct {
@@ -66,14 +68,14 @@ func Server() {
 	http.HandleFunc("/api/v1/calculate", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
-			json.NewEncoder(w).Encode(ResponseError{Error: "Method not allowed"})
+			json.NewEncoder(w).Encode(ResponseError{Error: "Method not allowed", Code: 405})
 			return
 		}
 
 		var req Request
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			w.WriteHeader(http.StatusUnprocessableEntity)
-			json.NewEncoder(w).Encode(ResponseError{Error: "Expression is not valid"})
+			json.NewEncoder(w).Encode(ResponseError{Error: "Expression is not valid", Code: 422})
 			fmt.Println(err)
 			return
 		}
@@ -83,17 +85,17 @@ func Server() {
 			switch err.Error() {
 			case "недопустимый символ", "пустое выражение", "несоответствующая скобка", "ошибка в выражении", "ошибка - недостаточно данных для вычисления", "ошибка - деление на ноль", "ошибка - неизвестная операция":
 				w.WriteHeader(http.StatusUnprocessableEntity)
-				json.NewEncoder(w).Encode(ResponseError{Error: "Expression is not valid"})
+				json.NewEncoder(w).Encode(ResponseError{Error: "Expression is not valid", Code: 405})
 				fmt.Println(err)
 			default:
 				w.WriteHeader(http.StatusInternalServerError)
-				json.NewEncoder(w).Encode(ResponseError{Error: "Internal server error"})
+				json.NewEncoder(w).Encode(ResponseError{Error: "Internal server error", Code: 500})
 			}
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(ResponseSuccess{Result: floatToString(result)})
+		json.NewEncoder(w).Encode(ResponseSuccess{Result: floatToString(result), Code: 200})
 	})
 	log.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
